7390c9d2f16d53aa824a82e1d7ac368581fb9602
yishai guedj <yishaiguedj1@gmail.com>
2026-09-23 12:00:07 +0300

    v32: כפתור ניקוי מטמון ורענון בהגדרות

--- tests/pension-kinds.test.js
+++ tests/pension-kinds.test.js
@@ -88,7 +88,7 @@ for (const id of ['newFundName', 'newFundKind', 'pensionFundAdd']) {
   ok(html.includes('id="' + id + '"'), 'index.html מכיל #' + id);
 }
 const sw = fs.readFileSync(path.join(__dirname, '..', 'sw.js'), 'utf8');
-ok(sw.includes('portfolio-pwa-v31'), 'sw.js בגרסת v31');
-ok(src.includes("const APP_VERSION = 'v31'"), 'APP_VERSION v31');
+ok(sw.includes('portfolio-pwa-v32'), 'sw.js בגרסת v32');
+ok(src.includes("const APP_VERSION = 'v32'"), 'APP_VERSION v32');
 
 console.log(`\nכל הבדיקות עברו ✓ (סה"כ אסרטים: ${n})`);
